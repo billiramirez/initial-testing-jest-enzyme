ff763de3111125d42479ed30fc713f6609a86f06
Billi Ramirez <ramirezbilly42@outlook.com>
2019-09-23 10:26:37 -0600

    refactoring

--- src/App.js
+++ src/App.js
@@ -13,15 +13,8 @@ class App extends React.Component {
 
   onClickButton = (action)=> {
     const {counter, error} = this.state;
-    // if(counter < 0 && action === 'inc'){
-    //   this.setState({error: !error})
-    // }
-    if(action === 'inc') {
-      if(error){
-        this.setState({counter: counter + 1, error: false});
-      }else {
-        this.setState({counter: counter + 1});
-      }
+    if(action) {
+      error ? this.setState({counter: counter + 1, error: false}) : this.setState({counter: counter + 1});
     } else{
       if(counter < 0 || counter - 1 < 0){
         this.setState({error: true});      
@@ -31,13 +24,12 @@ class App extends React.Component {
     }
   }
   render(){
-    console.log(this.state.counter);
     return (<div data-test="component-app">
       <h1 data-test="counter-display">the counter is currently {this.state.counter} </h1>
       <button data-test="increment-button" onClick={() => this.onClickButton('inc')}>
         Increment
       </button>
-      <button data-test="decrement-button" onClick={() => this.onClickButton('dec')}>
+      <button data-test="decrement-button" onClick={() => this.onClickButton()}>
         Decrement
       </button>
       { 
